f51e35994f95a3159d98f1ea294d2b5269a06d06
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-12-07 17:34:27 +0100

    added routes and controllers

---
+++ controllers/postController.js
@@ -0,0 +1,27 @@
+const Post = require("../models/post");
+
+// Display Post create form on GET.
+exports.post_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: Post create GET");
+};
+
+// Handle Post create on POST
+exports.post_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: Post create POST");
+};
+
+// Display list of all POST.
+exports.post_list = (req, res) => {
+    res.send("NOT IMPLEMENTED: Post list");
+};
+
+// Display Post delete form on GET.
+exports.post_delete_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: Post delete GET");
+};
+  
+// Handle Post delete on POST.
+exports.post_delete_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: Post delete POST");
+};
+
---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require("../models/user");
+
+// Display User create form on GET.
+exports.user_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: User create GET");
+};
+
+// Handle User create on POST.
+exports.user_create_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: User create POST");
+};
+
+
+
+
+// Display User update form on GET.
+exports.author_update_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: User update GET");
+};
+  
+// Handle User update on POST.
+exports.author_update_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: User update POST");
+};
+
+
+
+
+// Handle User membership code on GET.
+exports.user_membership_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: Membership create GET")
+}
+
+// Handle User membership code on POST.
+exports.user_membership_create_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: Membership create POST")
+}
+
+// Handle User admin code on GET.
+exports.user_admin_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: Admin create GET")
+}
+
+// Handle User admin code on POST.
+exports.user_admin_create_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: Admin create POST")
+}
