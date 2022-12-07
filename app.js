0e0b57354d4a208f5e1ed5957e98597be51b5f48
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-12-07 23:47:54 +0100

    implemented routes for users

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+// Require user controller module.
+const user_controller = require("../controllers/userController");
+
+// GET request for creating a user
+router.get("/create", user_controller.user_create_get);
+
+// POST request for creating a user
+router.post("/create", user_controller.user_create_post);
+
+// GET request for updating user membership status
+router.post("/:id/update", user_controller.user_membership_update_get);
+
+// POST request for updating user membership status
+router.post("/:id/update", user_controller.user_membership_upate_post);
+
+// GET request for updating user admin status
+router.post("/:id/update", user_controller.user_admin_update_get);
+
+// POST request for updating user adming status
+router.post("/:id/update", user_controller.user_admin_update_post);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
